d125874af0788c014960438beda79ea582ee01f3
eunhak lee <leftwing871@gmail.com>
2018-04-09 13:55:54 +0900

    ...

---
+++ src/honux/calandar/Sum.java
@@ -0,0 +1,31 @@
+package honux.calandar;
+
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		int a, b;
+		Scanner scanner = new Scanner(System.in);
+		
+		String s1, s2;
+		
+		System.out.println("두 수를 입력해 주세요.");
+		s1 = scanner.next();
+		s2 = scanner.next();
+		
+		System.out.println(s1 + ", " + s2);
+		
+		a = Integer.parseInt(s1);
+		b = Integer.parseInt(s2);
+		
+		System.out.println(a + ", " + b);
+		System.out.println("두 수의 합은 " + (a + b) + "입니다.");
+		
+		
+		System.out.printf("두 수의 합은 %d 입니다.", a + b);
+		
+		scanner.close();
+		
+	}
+}
+ 
